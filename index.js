69d774265864e6f05f4e63b00a6dc65278ea0820
Murylo Ferreira <128106455+Maurus2509@users.noreply.github.com>
2025-03-24 09:54:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const scanner = new Html5QrcodeScanner('reader', {
+    qrbox: {
+        width: 250,
+        height: 250
+    },
+    fps: 20,
+});
+
+const success = (result) => {
+    const qrdados = result;
+    const partes = qrdados.split(" - ");
+
+    const nome = partes[0];
+    const email = partes[1];
+
+    document.getElementById('result').innerHTML = `<h2>Success</h2>
+    <p>${nome}</p>
+    <p>${email}</p>
+    <button id="btn">Adicionar Curso 1</button>
+    `;
+
+    const post = async () => {
+        const endpoint = "http://localhost:8080/alunos/new";
+
+        fetch(endpoint, {
+            method: "POST",
+            body: JSON.stringify({
+                nome: nome,
+                email: email
+            }),
+            headers: {
+                "Content-Type":
+                    "application/json"
+            }
+        });
+    }
+
+    btn.addEventListener("click", () => {
+        post(qrdados);
+    });
+
+    scanner.clear();
+    document.getElementById('reader').remove();
+}
+
+const error = (error) => {
+    console.log(error);
+};
+
+scanner.render(success, error);
