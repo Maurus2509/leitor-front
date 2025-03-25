e69abaf4084059f452c72fecc5cd9317a7f5c71d
Murylo Ferreira <128106455+Maurus2509@users.noreply.github.com>
2025-03-25 09:45:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -35,29 +35,10 @@ const success = (result) => {
         });
     }
 
-    function initScanner() {
-        // Cria um novo elemento div para o scanner
-        const readerDiv = document.createElement('div');
-        readerDiv.id = 'reader';
-        document.body.appendChild(readerDiv);
-
-        // Recria o scanner
-        const newScanner = new Html5QrcodeScanner('reader', {
-            qrbox: {
-                width: 250,
-                height: 250
-            },
-            fps: 20,
-        });
-        newScanner.render(success, error);
-    }
 
     btn.addEventListener("click", () => {
         post(qrdados);
 
-        document.getElementById('result').innerHTML = '';
-
-        initScanner();
 
     });
 
