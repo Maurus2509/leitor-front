3b33e21ec316bd11f527112402774e46d17a7554
Murylo Ferreira <128106455+Maurus2509@users.noreply.github.com>
2025-03-25 09:53:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -35,8 +35,27 @@ const success = (result) => {
         });
     }
 
+    function initScanner() {
+        const readerDiv = document.createElement('div');
+        readerDiv.id = 'reader';
+        document.body.appendChild(readerDiv);
+
+        const newScanner = new Html5QrcodeScanner('reader', {
+            qrbox: {
+                width: 250,
+                height: 250
+            },
+            fps: 20,
+        });
+        newScanner.render(success, error);
+    }
+
     btn.addEventListener("click", () => {
         post(qrdados);
+
+        document.getElementById('result').innerHTML = '';
+
+        initScanner();
     });
 
     scanner.clear();
